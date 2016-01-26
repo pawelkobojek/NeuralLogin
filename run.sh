f35811d60731f1eb82b9952427d1bccc153d9b8d
Paweł Kobojek <PawelKobojek@gmail.com>
2016-01-26 13:33:41 +0100

    updated run script

--- run.sh
+++ run.sh
@@ -1,2 +1,6 @@
+if [ ! -e "emails.txt" ] || [ ! -e "training_data" ]; then
+  curl -L -o data.zip "https://www.dropbox.com/s/e1f6retimzix1xx/data.zip?dl=1" && unzip -n data.zip
+fi
+
 # Yeah, yeah, I know... but please, keep in mind that this is do-it-fast uni project
 cd training/keras && python learn_and_evaluate.py ; cd ../..
